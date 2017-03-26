4c3fbaaaa26399c95726a07c7434b63568b29ac1
Ioulia <markou@dtu.dk>
2017-03-27 01:38:59 +0200

    Finished. G

---
+++ javascript/Assignment2b.js
@@ -0,0 +1,260 @@
+function Assignment_2B() {
+
+    var color_list = ['DarkRed', 'gold', 'PaleVioletRed', 'DarkOrange', 'DarkGreen', 'MidnightBlue'];
+
+    var projection = d3.geo.mercator();
+    var current_clicked = 2;
+    var border = 2;
+    var bordercolor = 'black';
+    var margin = {
+        top: 50
+        , right: 5
+        , bottom: 5
+        , left: 50
+    };
+    var width = 1000 - margin.left - margin.right;
+    var height = 600 - margin.top - margin.bottom;
+
+    var lattop = 37.831575;
+    var lonleft = -122.514949;
+    var lonright = -120.906983;
+
+    var scale = 360 * width / (lonright - lonleft);
+    projection.scale(scale);
+
+    projection.translate([0, 0]);
+    // check where your top left coordinate is projected
+    var trans = projection([lonleft, lattop]);
+    // translate your map in the negative direction of that result
+    projection.translate([-1 * trans[0], -1 * trans[1]]);
+    var path = d3.geo.path()
+        .projection(projection);
+
+
+//            Create SVG element
+    svg = d3.select("#assignment2BSVG")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    svg.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("height", height)
+        .attr("width", width)
+        .style("stroke", bordercolor)
+        .style("fill", "PaleTurquoise")
+        .style("stroke-width", border);
+
+    svg.append("g").attr("id", "districts");
+
+    // geojson data of SF
+    d3.json("/10/data/sfpddistricts.geojson", function (json) {
+
+        svg.selectAll("path")
+            .data(json.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr({
+                "d": path,
+                'fill': "PeachPuff",
+                'opacity': 0.6,
+                'stroke-width': 1.5,
+                'stroke': 'black'
+            })
+            .on('mouseover', function () {
+                // Add a hover effect
+                d3.select(this)
+                    .transition()
+                    .attr('fill', 'Coral')
+            })
+            .on('mouseout', function () {
+                // Remove the hover effect
+                d3.select(this).transition()
+                    .attr('fill', "PeachPuff")
+            })
+            .append('title')
+            // Add tool tip, containing districts
+            .text(function (data) {
+                return data.properties.DISTRICT;
+            });
+
+        // district label in center of coordinates
+        svg.select("#districts").selectAll("text")
+            .data(json.features)
+            .enter()
+            .append("svg:text")
+            .attr("x", function (data) {
+                return path.centroid(data)[0];
+            })
+            .attr("y", function (data) {
+                return path.centroid(data)[1];
+            })
+            .attr("text-anchor", "middle")
+            .attr('font-size', '15px')
+            .attr('font-family', 'sans-serif')
+            .attr('fill', 'black');
+    });
+    var buttons = document.getElementsByClassName("button1");
+    var buttonsCount = buttons.length;
+
+    d3.json("/10/data/prost_pointssosto.json", function (data) {
+
+        svg.selectAll("circles")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("id", "points")
+            .attr("cx", function (d) {
+                return projection(d)[0];
+            })
+            .attr("cy", function (d) {
+                return projection(d)[1];
+            })
+            .attr("r", 5)
+            .style("fill", function (d) {
+                    return color_list[parseInt(d[2])];//K = 6
+                }
+            );
+
+        centClust(2);
+
+        for (var i = 0; i < buttonsCount; i += 1) {
+            buttons[i].onclick = clicked(i + 2);
+        }
+
+        function clicked(i) {
+            // alert(i);
+            d3.select("#K" + i)
+                .on("click", function () {
+                    current_clicked = i;
+                    centClust(current_clicked);
+
+                    svg.selectAll("#points")
+                        .style("fill", function (d) {
+                            return color_list[parseInt(d[i])];
+                        })
+                });
+        }
+    });
+
+    function hoverover() {
+        d3.json("/10/data/prost_pointssosto.json", function (data) {
+
+            d3.select("#K3")
+                .on("mouseover", function () {
+                    centClust(3);
+                    svg.selectAll("#points")
+                        .style("fill", function (d) {
+                            return color_list[parseInt(d[3])];
+                        })
+                })
+                .on("mouseout", function () {
+                    centClust(current_clicked);
+                    svg.selectAll("#points")
+                        .style("fill", function (d) {
+                            return color_list[parseInt(d[current_clicked])];
+                        })
+                });
+
+            d3.select("#K2")
+                .on("mouseover", function () {
+                    centClust(2);
+                    svg.selectAll("#points")
+                        .style("fill", function (d) {
+                            return color_list[parseInt(d[2])];
+                        })
+                })
+                .on("mouseout", function () {
+                    centClust(current_clicked);
+                    svg.selectAll("#points")
+                        .style("fill", function (d) {
+                            return color_list[parseInt(d[current_clicked])];
+                        })
+                });
+
+            d3.select("#K4")
+                .on("mouseover", function () {
+                    centClust(4);
+                    svg.selectAll("#points")
+                        .style("fill", function (d) {
+                            return color_list[parseInt(d[4])];
+                        })
+                })
+                .on("mouseout", function () {
+                    centClust(current_clicked);
+                    svg.selectAll("#points")
+                        .style("fill", function (d) {
+                            return color_list[parseInt(d[current_clicked])];
+                        })
+                });
+
+            d3.select("#K5")
+                .on("mouseover", function () {
+                    centClust(5);
+                    svg.selectAll("#points")
+                        .style("fill", function (d) {
+                            return color_list[parseInt(d[5])];
+                        })
+                })
+                .on("mouseout", function () {
+                    centClust(current_clicked);
+                    svg.selectAll("#points")
+                        .style("fill", function (d) {
+                            return color_list[parseInt(d[current_clicked])];
+                        })
+                });
+
+            d3.select("#K6")
+                .on("mouseover", function () {
+                    centClust(6);
+                    svg.selectAll("#points")
+                        .style("fill", function (d) {
+                            return color_list[parseInt(d[6])];
+                        })
+                })
+                .on("mouseout", function () {
+                    centClust(current_clicked);
+                    svg.selectAll("#points")
+                        .style("fill", function (d) {
+                            return color_list[parseInt(d[current_clicked])];
+                        })
+                })
+        })
+    }
+
+    function centClust(current_clicked) {
+
+        d3.json("/10/data/clusterCenterssosto.json", function (data) {
+            // if (data[2]==current_clicked)
+
+            svg.selectAll("#centers").remove();
+            svg.selectAll("clust")
+
+                .data(data)
+                .enter()
+                .append("circle")
+                .attr("id", "centers")
+                .attr("cx", function (d) {
+                    return projection(d)[0];
+                })
+                .attr("cy", function (d) {
+                    return projection(d)[1];
+                })
+                .attr("r", 7)
+                .attr("fill", 'none')
+                .style('stroke', 'black')
+                .style('stroke-width', function (d) {
+                    if (d[2] == current_clicked) {
+                        return 5;
+                    }
+                    else {
+                        return 0;
+                    }
+                })
+        });
+    }
+}
+
+Assignment_2B();
---
+++ javascript/assignment2a.js
@@ -0,0 +1,235 @@
+/**
+ * Created by imarkou on 27/03/2017.
+ */
+function Assignment_2A() {
+
+    var returnedObject = {
+        buttonClickEvent: function (filename) {
+            //ReadFileAndUpdateChart(filename);
+            if (filename === 'counts2015.txt') {
+                d3.select("#dataHeader").text("Data 2015");
+            }
+            else {
+                d3.select("#dataHeader").text("Data 2003");
+            }
+
+
+            d3.json(filename, function (data) {
+
+                data.forEach(function (item) {
+                    item.prostitution = parseInt(item.prostitution);
+                    item.vehicleThefts = parseInt(item.vehicleThefts);
+                    item.totalCrimes = parseInt(item.totalCrimes);
+                });
+
+                globalData = data;
+
+                console.log(globalData);
+
+
+                var xScale = d3.scale.linear()
+                    .domain([0, d3.max(globalData, function (data) {
+                        return data.prostitution;
+                    })])
+                    .range([padding, chartWidth - padding]);
+
+                var yScale = d3.scale.linear()
+                    .domain([0, d3.max(globalData, function (data) {
+                        return data.vehicleThefts;
+                    })])
+                    .range([chartHeight - padding, padding]);
+
+                const factor = 0.009;
+                const maxRadius = Math.sqrt(chartWidth * chartHeight * factor / Math.PI);
+                var rScale = d3.scale.linear()
+                    .domain([0, d3.max(globalData, function (data) {
+                        return data.totalCrimes;
+                    })])
+                    .range([0, maxRadius]);
+                UpdateCircles();
+                UpdateLabels();
+
+                function UpdateCircles() {
+                    var circles = svg.selectAll("circle.dataCircle")
+                        .data(globalData)
+                        .transition()
+                        .duration(1500)
+                        .attr("cx", function (data) {
+                            return xScale(data.prostitution);
+                        })
+                        .attr("cy", function (data) {
+                            return yScale(data.vehicleThefts);
+                        })
+                        .attr("r", function (data) {
+                            return rScale(data.totalCrimes);
+                        })
+                        .attr('opacity', 0.3);
+                }
+
+
+                function UpdateLabels() {
+                    //Add  labels
+                    svg.selectAll("text.label")
+                        .data(globalData)
+                        .transition()
+                        .duration(1500)
+                        .text(function (data) {
+                            return data.district;
+                        })
+                        .attr("x", function (data) {
+                            return xScale(data.prostitution);
+                        })
+                        .attr("y", function (data) {
+                            return yScale(data.vehicleThefts);
+                        })
+                        .attr("font-family", "sans-serif")
+                        .attr("font-size", "10px")
+                        .attr("fill", "#483D8B")
+                        .attr('opacity', 1);
+                }
+            });
+        }
+    }
+
+    var chartWidth = document.getElementById("assignment2ASVG").clientWidth;
+    var chartHeight = document.getElementById("assignment2ASVG").clientHeight;
+    const padding = 50;
+
+    //Create SVG element
+    var svg = d3.select("#assignment2ASVG");
+
+    var globalData;
+
+    //Data
+    function ReadFileAndUpdateChart(filename) {
+
+        d3.json(filename, function (data) {
+
+            data.forEach(function (item) {
+                item.prostitution = parseInt(item.prostitution);
+                item.vehicleThefts = parseInt(item.vehicleThefts);
+                item.totalCrimes = parseInt(item.totalCrimes);
+            });
+
+            globalData = data;
+
+            console.log(globalData);
+
+            var xScale = d3.scale.linear()
+                .domain([0, d3.max(data, function (data) {
+                    return data.prostitution;
+                })])
+                .range([padding, chartWidth - padding]);
+
+            var yScale = d3.scale.linear()
+                .domain([0, d3.max(data, function (data) {
+                    return data.vehicleThefts;
+                })])
+                .range([chartHeight - padding, padding]);
+
+            const factor = 0.009;
+            const maxRadius = Math.sqrt(chartWidth * chartHeight * factor / Math.PI);
+            var rScale = d3.scale.linear()
+                .domain([0, d3.max(data, function (data) {
+                    return data.totalCrimes;
+                })])
+                .range([0, maxRadius]);
+
+
+            DrawCircles();
+            AddLabels();
+            CreateAxes();
+
+            function DrawCircles() {
+                var circles = svg.selectAll("circle.dataCircle")
+                    .data(globalData)
+                    .enter()
+                    .append("circle")
+                    .attr("cx", function (data) {
+                        return xScale(data.prostitution);
+                    })
+                    .attr("cy", function (data) {
+                        return yScale(data.vehicleThefts);
+                    })
+                    .attr("r", function (data) {
+                        return rScale(data.totalCrimes);
+                    })
+                    .attr("class", "dataCircle")
+                    .attr('opacity', 0.3);
+            }
+
+
+            function AddLabels() { //Add  labels
+                svg.selectAll("text.label")
+                    .data(globalData)
+                    .enter()
+                    .append("text")
+                    .text(function (data) {
+                        return data.district;
+                    })
+                    .attr("x", function (data) {
+                        return xScale(data.prostitution);
+                    })
+                    .attr("y", function (data) {
+                        return yScale(data.vehicleThefts);
+                    })
+                    .attr("font-family", "sans-serif")
+                    .attr("font-size", "10px")
+                    .attr("fill", "#483D8B")
+                    .attr("class", "label")
+                    .attr('opacity', 1);
+            }
+
+            function CreateAxes() {
+                //Define X axis
+                var xAxis = d3.svg.axis()
+                    .scale(xScale)
+                    .orient("bottom")
+                    .ticks(10);
+
+                //Define Y axis
+                var yAxis = d3.svg.axis()
+                    .scale(yScale)
+                    .orient("left")
+                    .ticks(10);
+
+                //Create X axis
+                svg.append("g")
+                    .attr("class", "axis")
+                    .attr("transform", "translate(0," + (chartHeight - padding - 6) + ")")
+                    .call(xAxis);
+
+
+                //Create Y axis
+                svg.append("g")
+                    .attr("class", "axis")
+                    .attr("transform", "translate(" + padding + ",0)")
+                    .call(yAxis);
+
+                //Add text
+                svg.append("text")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 0 - padding + 15)
+                    .attr("x", 0 - (chartHeight / 2))
+                    .attr("dy", "3em")
+                    .style("text-anchor", "middle")
+                    .text("Vehicle-Theft Crimes");
+
+                // Add the text label for the x axis
+                svg.append("text")
+                    .attr("transform", "translate(" + (chartWidth / 2) + "," + (chartHeight - 15) + ")").style("text-anchor", "middle")
+                    .text("Prostitution Crimes");
+            }
+        });
+    }
+
+    ReadFileAndUpdateChart("/10/data/counts2003.json");
+
+    return returnedObject;
+
+}
+
+
+var assignment2AObject = Assignment_2A();
+//assignment2AObject.buttonClickEvent("/10/data/counts2003.json");
+console.log(assignment2AObject);
